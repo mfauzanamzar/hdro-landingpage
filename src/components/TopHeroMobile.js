bb7494dfcabbed6d0a1ab7d8aa50b6d5398792e1
mfauzanamzar <mfauzanamzar@gmail.com>
2022-02-27 01:59:02 +0800

    final

---
+++ src/components/TopHeroMobile.js
@@ -0,0 +1,13 @@
+import React from "react";
+import WorldMap from "../images/WorldMap2.png"
+
+const TopHeroMobile = () => {
+  return (
+      <div className="tophero-mobile">
+        <img src={WorldMap} alt="background" />
+      </div>
+   
+  );
+};
+
+export default TopHeroMobile;
